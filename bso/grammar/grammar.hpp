b6d8dd75cec82f5f81e9d10b10850ee2953e3b43
Jildert Turkstra <152167914+Jildert-Turkstra@users.noreply.github.com>
2023-12-18 14:39:53 +0100

    Add files via upload

---
+++ bso/grammar/grammar.hpp
@@ -0,0 +1,108 @@
+#ifndef BSO_GRAMMAR_HPP
+#define BSO_GRAMMAR_HPP
+
+#include <bso/spatial_design/cf_building.hpp>
+#include <bso/grammar/rule_set/rule_sets.hpp>
+
+namespace bso { namespace grammar {
+
+
+#ifdef SD_MODEL_HPP
+class DEFAULT_SD_GRAMMAR;
+#endif
+
+#ifdef BSO_BP_MODEL_HPP
+class DEFAULT_BP_GRAMMAR;
+#endif
+
+class grammar
+{
+private:
+	spatial_design::cf_building mCFBuilding;
+
+	// rule sets
+	std::map<spatial_design::conformal::cf_vertex*, rule_set::vertex_property*> mVertexProperties;
+	std::map<spatial_design::conformal::cf_line*, rule_set::line_property*> mLineProperties;
+	std::map<spatial_design::conformal::cf_rectangle*, rule_set::rectangle_property*> mRectangleProperties;
+	std::map<spatial_design::conformal::cf_cuboid*, rule_set::cuboid_property*> mCuboidProperties;
+
+	std::map<spatial_design::conformal::cf_point*, rule_set::point_property*> mPointProperties;
+	std::map<spatial_design::conformal::cf_edge*, rule_set::edge_property*> mEdgeProperties;
+	std::map<spatial_design::conformal::cf_surface*, rule_set::surface_property*> mSurfaceProperties;
+	std::map<spatial_design::conformal::cf_space*, rule_set::space_property*> mSpaceProperties;
+
+	#ifdef SD_MODEL_HPP
+	std::map<spatial_design::conformal::cf_vertex*, rule_set::sd_vertex_rule*> mSDVertexRules;
+	std::map<spatial_design::conformal::cf_line*, rule_set::sd_line_rule*> mSDLineRules;
+	std::map<spatial_design::conformal::cf_rectangle*, rule_set::sd_rectangle_rule*> mSDRectangleRules;
+	std::map<spatial_design::conformal::cf_cuboid*, rule_set::sd_cuboid_rule*> mSDCuboidRules;
+
+	structural_design::sd_model mSDModel;
+	std::vector<structural_design::sd_model> mIntermediateSDModels;
+	std::map<std::pair<std::string,std::string>,
+		structural_design::component::structure> mSDWallProperties;
+	std::map<std::pair<std::string,std::string>,
+		structural_design::component::structure> mSDFloorProperties;
+    std::map<std::string, structural_design::component::structure> mSDSpaceProperties;
+	structural_design::component::structure mLoadPanel;
+	std::multimap<std::string, structural_design::component::load> mLoads;
+	unsigned int mMeshSize = 1;
+
+	void mReadSDSettings(const std::string& fileName);
+	#endif
+
+	#ifdef BSO_BP_MODEL_HPP
+	std::map<spatial_design::conformal::cf_space*, rule_set::bp_space_rule*> mBPSpaceRules;
+	std::map<spatial_design::conformal::cf_rectangle*, rule_set::bp_rectangle_rule*> mBPRectangleRules;
+
+	boost::posix_time::time_duration mBPWarmUpDuration;
+	boost::posix_time::time_duration mBPTimeStepSize;
+	double mBPGroundTemperature = 0;
+	std::map<std::string,boost::posix_time::time_period> mBPSimulationPeriods;
+	std::map<std::string,bso::building_physics::properties::space_settings> mBPSpaceSettings;
+	std::map<std::pair<std::string,std::string>,bso::building_physics::properties::construction> mBPWallConstructions;
+	std::map<std::pair<std::string,std::string>,bso::building_physics::properties::construction> mBPFloorConstructions;
+	std::map<std::pair<std::string,std::string>,bso::building_physics::properties::glazing> mBPWallGlazings;
+	std::map<std::pair<std::string,std::string>,bso::building_physics::properties::glazing> mBPFloorGlazings;
+
+	building_physics::bp_model mBPModel;
+	std::vector<building_physics::bp_model> mIntermediateBPModels;
+	void mReadBPSettings(const std::string& fileName);
+	#endif
+
+public:
+	grammar(const spatial_design::cf_building& cf);
+	~grammar();
+
+	#ifdef SD_MODEL_HPP
+	template <typename T = DEFAULT_SD_GRAMMAR, typename...ARGS>
+	bso::structural_design::sd_model sd_grammar(const ARGS&...);
+	const std::vector<structural_design::sd_model>& getIntermediateSDModels() const
+		{return mIntermediateSDModels;}
+	#endif
+
+	#ifdef BSO_BP_MODEL_HPP
+	template <typename T = DEFAULT_BP_GRAMMAR, typename...ARGS>
+	bso::building_physics::bp_model bp_grammar(const ARGS&...);
+	const std::vector<building_physics::bp_model> getIntermediateBPModels() const
+		{return mIntermediateBPModels;}
+	#endif
+};
+
+} // namespace grammar
+} // namespace bso
+
+#include <bso/grammar/grammar.cpp>
+
+#ifdef SD_MODEL_HPP
+#include <bso/grammar/grammar_sd_specifics.cpp>
+#include <bso/grammar/sd_grammars/default_sd_grammar.cpp>
+#include <bso/grammar/sd_grammars/volume_sd_grammar.cpp>
+#endif
+
+#ifdef BSO_BP_MODEL_HPP
+#include <bso/grammar/grammar_bp_specifics.cpp>
+#include <bso/grammar/bp_grammars/default_bp_grammar.cpp>
+#endif
+
+#endif // BSO_GRAMMAR_HPP
---
+++ bso/grammar/sd_grammars/volume_sd_grammar.cpp
@@ -0,0 +1,119 @@
+#ifndef BSO_GRAMMAR_VOLUME_SD_GRAMMAR_CPP
+#define BSO_GRAMMAR_VOLUME_SD_GRAMMAR_CPP
+
+#include <stdexcept>
+#include <sstream>
+
+namespace bso { namespace grammar {
+class VOLUME_SD_GRAMMAR;
+
+template <>
+bso::structural_design::sd_model grammar::sd_grammar<VOLUME_SD_GRAMMAR>(const std::string& fileName)
+{
+	// initialize a new SD model
+	mSDModel = bso::structural_design::sd_model();
+	// read the structural design settings
+	this->mReadSDSettings(fileName);
+
+	std::map<spatial_design::conformal::cf_cuboid*, 
+					 structural_design::component::structure> cuboidStructures;
+	for (const auto& i : mCuboidProperties)
+	{
+		// get the space type depending on the settings
+		std::string usedType = i.second->getSpaceType();
+
+		// determine the structural type and properties of the cuboid
+		bool structureFound = false;
+		auto structureSearch = mSDSpaceProperties.find(usedType);
+		if (i.second->isInSpace())
+		{
+			if (structureSearch != mSDSpaceProperties.end())
+			{
+				structureFound = true;
+			}
+		}
+
+		if (!structureFound)
+		{
+			std::stringstream errorMessage;
+			errorMessage << "\nError, in volume grammar could not find the specified\n"
+									 << "type settings: \"" << usedType << "\""
+									 << "Has this type set been specified in the inputfile?: \""
+									 << fileName << "\"\n(bso/grammar/sd_grammars/volume_sd_grammar.cpp)"
+									 << std::endl;
+			throw std::runtime_error(errorMessage.str());
+		}
+
+		// create a cuboid rule set
+		auto sdCuboidRuleSet = new rule_set::sd_cuboid_rule(i.second);
+		mSDCuboidRules[i.first] = sdCuboidRuleSet;
+		if (structureSearch->second.type() != "none")
+		{
+			sdCuboidRuleSet->assignStructure(structureSearch->second);
+			cuboidStructures.emplace(i.first,structureSearch->second);
+		}
+	}
+	
+	for (const auto& i : mRectangleProperties)
+	{
+		if (i.second->isSpaceSeparating())
+		{
+			// create a rectangle rule set
+			auto sdRectangleRuleSet = new rule_set::sd_rectangle_rule(i.second);
+			mSDRectangleRules[i.first] = sdRectangleRuleSet;
+			sdRectangleRuleSet->assignLoadPanel(mLoadPanel); // add a load panel, in case a load is assigned but no structure can carry it
+			sdRectangleRuleSet->addLoads(&mLoads); // add the loads
+			
+			std::vector<structural_design::component::structure> potentialStructures;
+			for (const auto& j : i.second->getRectanglePtr()->cfCuboids())
+			{
+				auto structureSearch = cuboidStructures.find(j);
+				if (structureSearch != cuboidStructures.end())
+				{
+					potentialStructures.push_back(structureSearch->second);
+				}
+			}
+			sdRectangleRuleSet->assignStructure(potentialStructures);
+		}
+	}
+	
+	// find each line that is associated to a rectangle that has a rectangle rule set
+	for (const auto& i : mSDRectangleRules)
+	{ // for each sd rectangle rule
+		for (const auto& j : i.first->cfLines())
+		{ // for each line associated to the rectangle associated to the rectangle rule
+			if (mSDLineRules.find(j) == mSDLineRules.end());
+			{ // if it has not yet been added to the vector, add it
+				mSDLineRules[j] = new rule_set::sd_line_rule(mLineProperties[j]); // create line rules, in this case only to generate boundary conditions
+			}
+		}
+	}
+
+	for (const auto& i : mVertexProperties)
+	{
+		if (i.second->isSpaceCorner())
+		{
+			mSDVertexRules[i.first] = new rule_set::sd_vertex_rule(i.second); // create nodes at space corners
+		}
+	}
+
+	// apply the rules
+	for (const auto& i : mSDVertexRules) i.second->apply(mSDModel);
+	for (const auto& i : mSDLineRules) i.second->apply(mSDModel);
+	for (const auto& i : mSDRectangleRules) i.second->apply(mSDModel);
+	for (const auto& i : mSDCuboidRules) i.second->apply(mSDModel);
+	mSDModel.setMeshSize(mMeshSize);
+
+	// delete the rules
+	for (auto& i : mSDVertexRules) delete i.second;
+	for (auto& i : mSDLineRules) delete i.second;
+	for (auto& i : mSDRectangleRules) delete i.second;
+	for (auto& i : mSDCuboidRules) delete i.second;
+
+	return mSDModel;
+} // volume_sd_grammar
+
+} // namespace grammar
+} // namespace bso
+
+#endif // BSO_GRAMMAR_VOLUME_SD_GRAMMAR_CPP
