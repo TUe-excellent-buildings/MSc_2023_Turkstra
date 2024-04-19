834bf40b2ec1b7ee033023804cf6a972492c0796
Jildert Turkstra <152167914+Jildert-Turkstra@users.noreply.github.com>
2024-04-19 21:32:05 +0200

    Add files via upload
    Added getSubRectangles

--- bso/structural_design/sd_model.hpp
+++ bso/structural_design/sd_model.hpp
@@ -3,6 +3,8 @@
 
 #include <ostream>
 #include <sstream>
+#include <vector>
+#include <map>
 #include <bso/structural_design/fea.hpp>
 #include <bso/structural_design/component/point.hpp>
 #include <bso/structural_design/component/line_segment.hpp>
@@ -10,19 +12,22 @@
 #include <bso/structural_design/component/quad_hexahedron.hpp>
 
 namespace bso { namespace structural_design {
-	
+
 	struct sd_results;
-	
+
 	class sd_model
 	{
 	private:
 		std::vector<component::point*> mPoints;
 		std::vector<component::geometry*> mGeometries;
 		std::vector<component::point*> mMeshedPoints;
-		
+
+		// Use a vector to store pointers to subRectangles
+        std::vector<bso::spatial_design::conformal::cf_rectangle*> mSubRectangles;
+
 		fea* mFEA;
 		std::streambuf* mTopOptStreamBuffer;
-		
+
 		unsigned int mMeshSize = 1;
 		bool mIsMeshed = false;
 		void clearMesh();
@@ -30,35 +35,44 @@ namespace bso { namespace structural_design {
 		sd_model();
 		sd_model(const sd_model& rhs);
 		~sd_model();
-		
+
 		component::point* addPoint(bso::utilities::geometry::vertex p);
 		component::geometry* addGeometry(const bso::utilities::geometry::line_segment& g);
 		component::geometry* addGeometry(const bso::utilities::geometry::quadrilateral& g);
 		component::geometry* addGeometry(const bso::utilities::geometry::quad_hexahedron& g);
-		
+
 		void setMeshSize(const unsigned int& n);
 		void mesh();
 		void mesh(const unsigned int& n, bool meshLoadPanels = true);
 		void analyze(std::string solver = "SimplicialLDLT");
 		bool isStable();
-		
+
 		void rescaleStructuralVolume(const double& scaleFactor);
 		void setElementDensities(const double& volumeFraction, const double& penalty);
-		
+
 		template <typename T, typename...ARGS>
 		void topologyOptimization(const ARGS&...);
 		void setTopOptOutputStream(std::ostream& out);
-		
+
 		sd_results getTotalResults();
 		sd_results getPartialResults(bso::utilities::geometry::polygon* geom);
 		sd_results getPartialResults(bso::utilities::geometry::polyhedron* geom);
-		
+
 		fea* getFEA() {return mFEA;}
 		fea* const getFEA() const {return mFEA;}
 		const std::vector<component::point*>& getPoints() const {return mPoints;}
 		const std::vector<component::geometry*> getGeometries() const {return mGeometries;}
+
+        // Methods to manage subRectangles
+        void addSubRectangle(bso::spatial_design::conformal::cf_rectangle* rectangle) {
+            mSubRectangles.push_back(rectangle);
+        }
+
+        const std::vector<bso::spatial_design::conformal::cf_rectangle*>& getSubRectangles() const {
+            return mSubRectangles;
+        }
 	};
-	
+
 	struct sd_results
 	{
 		double mTotalStrainEnergy = 0.0;
@@ -70,7 +84,7 @@ namespace bso { namespace structural_design {
 		double mGhostStructuralVolume = 0.0;
 		// std::map<element::load_case,double> strainEnergyPerLoadCase = {};
 	};
-	
+
 } // namespace structural_design
 } // namespace bso
 
